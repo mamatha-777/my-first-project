570697e40e3be113e114c0cab891ee45ec99d542
Mamatha <valkemamathayadav777@gmail.com>
2026-05-01 23:57:41 +0530

    created a new branch in local repository mamatha-777-patch-3 changes were made only in program1 file

--- my-first-project/src/main/java/my_first_project/my_first_project/Program1.java
+++ my-first-project/src/main/java/my_first_project/my_first_project/Program1.java
@@ -4,6 +4,7 @@ public class Program1 {
 	public static void main(String[] args) {
 		
 		System.out.println("my first program");
+		System.out.println("commiting the code into the new branch mamatha-777-patch-3");
 	}
 
 }
