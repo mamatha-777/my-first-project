45c331181ba328a91d93308c279047834e4f396f
mamatha-777 <55461750+mamatha-777@users.noreply.github.com>
2026-05-01 21:36:48 +0530

    Update Program2.java

--- my-first-project/src/main/java/my_first_project/my_first_project/Program2.java
+++ my-first-project/src/main/java/my_first_project/my_first_project/Program2.java
@@ -3,6 +3,7 @@ package my_first_project.my_first_project;
 public class Program2 {
 	public static void main(String[] args) {
 		System.out.println("program 2");
+		System.out.println("adding the line to pull the code in the local machine");
 	}
 
 }
