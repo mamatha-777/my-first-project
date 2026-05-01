34dcaa80d6512d4535b5abe3a6231f54529ac855
Mamatha <valkemamathayadav777@gmail.com>
2026-05-01 23:49:26 +0530

    made changes in program 2 and pushing the changes to the mamatha-777-patch-1 branch

--- my-first-project/src/main/java/my_first_project/my_first_project/Program2.java
+++ my-first-project/src/main/java/my_first_project/my_first_project/Program2.java
@@ -4,6 +4,7 @@ public class Program2 {
 	public static void main(String[] args) {
 		System.out.println("program 2");
 		System.out.println("adding the line to pull the code in the local machine");
+		System.out.println("commiting the code into the old branch mamatha-777-patch-1");
 	}
 
 }
