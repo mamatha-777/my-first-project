0105df11e0d5b4401cee60f97965fcdcf2b27684
mamatha-777 <55461750+mamatha-777@users.noreply.github.com>
2026-05-01 21:54:50 +0530

    Update Program3.java

--- my-first-project/src/main/java/my_first_project/my_first_project/Program3.java
+++ my-first-project/src/main/java/my_first_project/my_first_project/Program3.java
@@ -3,6 +3,7 @@ package my_first_project.my_first_project;
 public class Program3 {
 	public static void main(String[] args) {
 		System.out.println("Program 3");
+			System.out.println("making chnges in the git main branch in repo");
 	}
 
 }
